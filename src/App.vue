3fb444b8409350f31f0227f3c0580eaf514c3adc
Bishal Budhapirthi <bish@Alexs-iMac.local>
2020-07-09 20:25:07 +0100

    Create crud appointment vue interface

---
+++ src/App.vue
@@ -0,0 +1,95 @@
+<template>
+  <div id="main-app" class="container">
+    <div class="row justify-content-center">
+      <add-appointment @add="addItem"/>
+      <search-appointments
+              @searchRecords="searchAppointments"
+             :myKey="filterKey"
+             :myDir="filterDir"
+              @requestKey="changeKey"
+              @requestDir="changeDir"
+      />
+      <appointment-list :appointments="filteredApts" @remove="removeItem" @edit="editItem"/>
+    </div>
+  </div>
+</template>
+
+<script>
+  import AppointmentList from "./components/AppointmentList";
+  import _ from "lodash";
+  import axios from "axios";
+  import AddAppointment from "./components/AddAppointment";
+  import SearchAppointments from "./components/SearchAppointments";
+
+export default {
+  name: 'MainApp',
+  data: function () {
+    return {
+      title: "Appointment List",
+      appointments : [],
+      filterKey: "petName",
+      filterDir: "desc",
+      searchTerms: "",
+      aptIndex: 0
+    };
+  },
+  components: {
+    AddAppointment,
+    AppointmentList,
+    SearchAppointments,
+  },
+  mounted() {
+    axios.get("./data/appointments.json")
+    .then(response => (this.appointments = response.data.map(item => {
+      item.aptId = this.aptIndex;
+      this.aptIndex++;
+      return item
+    })));
+  },
+  computed: {
+    searchedApts: function () {
+       return this.appointments.filter(item =>{
+         return (
+           item.petName.toLowerCase().match(this.searchTerms.toLowerCase()) ||
+           item.petOwner.toLowerCase().match(this.searchTerms.toLowerCase()) ||
+           item.aptNotes.toLowerCase().match(this.searchTerms.toLowerCase())
+         );
+       });
+    },
+    filteredApts: function () {
+      return _.orderBy(
+              this.searchedApts,
+              item => {
+                return item[this.filterKey].toLowerCase();
+              }, this.filterDir
+      );
+    }
+  },
+  methods: {
+    changeKey: function(value) {
+      this.filterKey = value;
+    },
+    changeDir: function(value) {
+      this.filterDir = value;
+    },
+    searchAppointments: function(terms) {
+      this.searchTerms = terms;
+    },
+    removeItem: function (apt) {
+      this.appointments = _.without(this.appointments, apt);
+    },
+    editItem: function (id, field, text) {
+      const aptIndex = _.findIndex(this.appointments, {
+        aptId: id
+      });
+      this.appointments[aptIndex][field] = text;
+    },
+    addItem: function (apt) {
+      apt.aptId =  this.aptIndex;
+      this.aptIndex++;
+      this.appointments.push(apt);
+    }
+  }
+}
+</script>
+
